c2f8afdd64e4cafed1f0d4245499e6f4d3aa4546
ethan-phillips26 <ethanphillips2005@gmail.com>
2025-12-06 16:58:51 -0600

    Initial commit with java project

---
+++ src/csci/pkg366/pkgfinal/project/CSCI366FinalProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package csci.pkg366.pkgfinal.project;
+
+/**
+ *
+ * @author ethan
+ */
+public class CSCI366FinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
